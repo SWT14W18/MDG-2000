0aa4e171cfd545c856872ab45f2da16f612e5f25
Reinhard <reinhard1994@aol.com>
2014-11-19 16:45:40 +0100

    LotterieController überarbeitet

--- src/main/java/de/tudresden/swt14ws18/controller/LotterieController.java
+++ src/main/java/de/tudresden/swt14ws18/controller/LotterieController.java
@@ -1,10 +1,18 @@
 package de.tudresden.swt14ws18.controller;
 
+import java.util.Optional;
+
+import org.salespointframework.useraccount.AuthenticationManager;
+import org.salespointframework.useraccount.Password;
+import org.salespointframework.useraccount.UserAccount;
+import org.salespointframework.useraccount.UserAccountIdentifier;
 import org.salespointframework.useraccount.UserAccountManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import de.tudresden.swt14ws18.useraccountmanager.CommunityRepository;
 import de.tudresden.swt14ws18.useraccountmanager.CustomerRepository;
@@ -15,9 +23,10 @@ public class LotterieController {
 	private final UserAccountManager userAccountManager;
 	private final CustomerRepository customerRepository;
 	private final CommunityRepository communityRepository;
+	private final AuthenticationManager authenticationManager;
 	
 	@Autowired
-	public LotterieController(UserAccountManager userAccountManager, CustomerRepository customerRepository, CommunityRepository communityRepository){
+	public LotterieController(UserAccountManager userAccountManager, CustomerRepository customerRepository, CommunityRepository communityRepository, AuthenticationManager authenticationManager){
 		
 		Assert.notNull(userAccountManager, "UserAccountManager must not be null!");
 		Assert.notNull(customerRepository, "CustomerRepository must not be null!");
@@ -26,6 +35,7 @@ public class LotterieController {
 		this.userAccountManager = userAccountManager;
 		this.customerRepository = customerRepository;
 		this.communityRepository = communityRepository;
+		this.authenticationManager = authenticationManager;
 	}
 	@RequestMapping("/")
 	public String Toindex(){
@@ -86,9 +96,37 @@ public class LotterieController {
 		return "logout";
 	}
 	
-	@RequestMapping("/registration")
-	public String registration(){
-		return "registration";
+//	@RequestMapping("/registration")
+//	public String registration(){
+//		return "register";
+//	}
+	
+	@RequestMapping({"/registration"})
+	public String register(@RequestParam("username") String vorname, @RequestParam("password") String passwort, ModelMap modelMap){
+		if(userAccountManager.contains(new UserAccountIdentifier(vorname))){
+			modelMap.addAttribute("registrationError", true);
+			return "registration";
+		}
+		userAccountManager.save(userAccountManager.create(vorname, passwort));
+		return "index";
+	}
+	
+	@RequestMapping({"/input"})
+	public String input(@RequestParam("vorname") String vorname, @RequestParam("passwort") String passwort, ModelMap modelMap){
+		Optional<UserAccount> user = userAccountManager.get(new UserAccountIdentifier(vorname));
+		
+		if(user.isPresent()){
+			
+		  if(authenticationManager.matches(new Password(passwort), user.get().getPassword())){
+		      
+			  
+			  return "index";//index();
+		  }
+		  
+		}
+		
+		modelMap.addAttribute("loginError",true);
+		return "index";
 	}
 	
 }
