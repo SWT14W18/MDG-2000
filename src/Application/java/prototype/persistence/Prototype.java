14809c31fd215d96ba40c17d19fae92db39d7186
ghostdogRL <joekarlshorst@yahoo.de>
2014-11-22 00:07:44 +0100

    retry

--- src/Application/java/prototype/persistence/Prototype.java
+++ src/Application/java/prototype/persistence/Prototype.java
@@ -1,28 +1,19 @@
 package prototype.persistence;
 
-import java.util.ArrayList;
-import java.util.List;
+//import java.util.ArrayList;
+//import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
 
 @Entity
 public class Prototype{
     
-    @Id
-    @GeneratedValue
-    private long id; 
+    @GeneratedValue 
     private double value = 0;
 	
-	
-    @OneToMany
-    private List<Prototype> prototypeList = new ArrayList<>();
-
     public void addingValue(Prototype count) {
 	value += count.getCounter();
-	prototypeList.add(count);
     }
 
     private double getCounter() {
@@ -36,18 +27,9 @@ public class Prototype{
 	
 	Prototype count = new Prototype();
 	value += counter;
-	prototypeList.add(count);
     }
 
     public double getValue() {
 	return value;
-    }
-
-    public List<Prototype> getprototypeList() {
-	return prototypeList;
-    }
-
-    public boolean hasValue(double value) {
-	return value >= value;
     }
 }
