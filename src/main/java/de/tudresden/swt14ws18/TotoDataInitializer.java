b6b0a1fd481bb903997e8eb5defb884fba95c2c1
Reinhard <reinhard1994@aol.com>
2014-11-24 14:13:27 +0100

    merge

--- src/main/java/de/tudresden/swt14ws18/TotoDataInitializer.java
+++ src/main/java/de/tudresden/swt14ws18/TotoDataInitializer.java
@@ -22,47 +22,44 @@ import de.tudresden.swt14ws18.gamemanagement.TotoMatch;
 import de.tudresden.swt14ws18.gamemanagement.TotoMatchRepository;
 import de.tudresden.swt14ws18.gamemanagement.TotoResult;
 
-
 public class TotoDataInitializer {
-	
 	private TotoMatchRepository totoMatchRepository;
-	
-	SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
-	
+	SimpleDateFormat inputSDF = new SimpleDateFormat("yyyy-MM-dd;HH:mm:ss");
 	public TotoDataInitializer(TotoMatchRepository totoMatchRepository){
 		this.totoMatchRepository = totoMatchRepository;
 	}
-		
-
+	
 	public void totoInitialize() throws IOException, ParseException{
-		
-		Map<TotoResult, Double> quotes = new HashMap<>();
-		quotes.put(TotoResult.DRAW, 2D);
-		quotes.put(TotoResult.WIN_GUEST, 2D);
-		quotes.put(TotoResult.WIN_HOME, 2D);
-		
+		// if (totoMatchRepository.findAll().iterator().hasNext()) {
+		// 	return;
+		// }
+	    Map<TotoResult, Double> quotes = new HashMap<>();
+	    quotes.put(TotoResult.DRAW, 2D);
+	    quotes.put(TotoResult.WIN_GUEST, 2D);
+	    quotes.put(TotoResult.WIN_HOME, 2D);
 	    URL url = new URL("http://openligadb-json.herokuapp.com/api/matchdata_by_league_saison?league_saison=2014&league_shortcut=bl1");
 	    HttpURLConnection request = (HttpURLConnection) url.openConnection();
 	    request.connect();
-
-	    JsonParser jp = new JsonParser(); 
+	    
+	    JsonParser jp = new JsonParser();
 	    JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
-	    JsonObject rootobj = root.getAsJsonObject(); 
+	    JsonObject rootobj = root.getAsJsonObject();
 	    JsonArray matches = (JsonArray) rootobj.get("matchdata");
+	    
 	    String team1;
 	    String team2;
 	    String date;
 	    Date gameDate;
-	    for (int i=0;i<306;i++){
-
-	    			JsonObject match = (JsonObject) matches.get(i);
-	    			team1 = match.get("name_team1").getAsString();	
-	    			team2 = match.get("name_team2").getAsString();
-	    			date = match.get("match_date_time").getAsString();
-	    			gameDate = sdf.parse(date.substring(8, 10)+"."+date.substring(5, 7)+"."+date.substring(0, 4));
-	    			totoMatchRepository.save(new TotoMatch(team1, team2, quotes, gameDate, TotoGameType.BUNDESLIGA1));
-
-	    }
+	    int matchDay;
 	    
+	    for (int i=0;i<306;i++){
+	    	JsonObject match = (JsonObject) matches.get(i);
+	    	team1 = match.get("name_team1").getAsString();
+	    	team2 = match.get("name_team2").getAsString();
+	    	date = match.get("match_date_time").getAsString();
+	    	matchDay = match.get("group_order_id").getAsInt();
+	    	gameDate = inputSDF.parse(date.substring(0, 10)+";"+date.substring(11, 19));
+	    	totoMatchRepository.save(new TotoMatch(team1, team2, quotes, gameDate, TotoGameType.BUNDESLIGA1, matchDay));
+	    }
 	}
 }
