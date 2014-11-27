76bb272ad8519173351ea44d1b194f05104e8c0f
SydMontague <sydmontague@web.de>
2014-11-27 18:49:24 +0100

    Mehr JavaDocs

--- src/main/java/de/tudresden/swt14ws18/gamemanagement/TotoGameType.java
+++ src/main/java/de/tudresden/swt14ws18/gamemanagement/TotoGameType.java
@@ -1,6 +1,18 @@
 package de.tudresden.swt14ws18.gamemanagement;
 
 public enum TotoGameType {
-	POKAL, BUNDESLIGA1, BUNDESLIGA2;
+    POKAL("DFB Pokal"),
+    BUNDESLIGA1("1. Bundesliga"),
+    BUNDESLIGA2("2. Bundesliga");
 
+    private String name;
+
+    private TotoGameType(String name) {
+	this.name = name;
+    }
+
+    @Override
+    public String toString() {
+	return name;
+    }
 }
