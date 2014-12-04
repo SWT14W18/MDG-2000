74b1bb0e72be582fad7887d742b56df1aa62fe68
SydMontague <sydmontague@web.de>
2014-12-04 18:28:54 +0100

    CommunityRepo änderung

--- src/main/java/de/tudresden/swt14ws18/repositories/CommunityRepository.java
+++ src/main/java/de/tudresden/swt14ws18/repositories/CommunityRepository.java
@@ -1,10 +1,15 @@
 package de.tudresden.swt14ws18.repositories;
 
+import java.util.List;
+
 import org.salespointframework.useraccount.UserAccount;
 import org.springframework.data.repository.CrudRepository;
 
 import de.tudresden.swt14ws18.useraccountmanager.Community;
+import de.tudresden.swt14ws18.useraccountmanager.ConcreteCustomer;
 
 public interface CommunityRepository extends CrudRepository<Community, Long> {
 	public Community findByUserAccount(UserAccount userAccount);
+	
+	public List<Community> findByMembersContains(ConcreteCustomer member);
 }
