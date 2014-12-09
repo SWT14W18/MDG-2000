e3152807a8a1a3e4d36ffd32586a7ddd703a7bf5
Reinhard <reinhard1994@aol.com>
2014-12-09 12:11:16 +0100

    da wird doch der Test verrückt -.-

--- src/test/java/de/tudresden/swt14ws18/MessageTest.java
+++ src/test/java/de/tudresden/swt14ws18/MessageTest.java
@@ -4,32 +4,34 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import org.salespointframework.useraccount.UserAccount;
+import org.salespointframework.useraccount.UserAccountManager;
 
 import de.tudresden.swt14ws18.bank.BankAccount;
 import de.tudresden.swt14ws18.gamemanagement.GameType;
+import de.tudresden.swt14ws18.repositories.*;
 import de.tudresden.swt14ws18.useraccountmanager.ConcreteCustomer;
 import de.tudresden.swt14ws18.useraccountmanager.Status;
 import de.tudresden.swt14ws18.util.Constants;
 
 public class MessageTest {
-
-       
-
-    private UserAccount userAccount = Lotterie.getInstance().getUserAccountManager()
-            .create("Dieter", "678", Constants.USER, Constants.CUSTOMER, Constants.CUSTOMER_BLOCKABLE);
-    private BankAccount bankAccount = new BankAccount();
+   
+    private final CustomerRepository customerRepository = Lotterie.getInstance().getCustomerRepository();
+    private final MessageRepository messageRepo = Lotterie.getInstance().getMessagesRepository();
+    private final UserAccountManager userAccountManager = Lotterie.getInstance().getUserAccountManager();
+    private final BankAccountRepository bankAccountRepository = Lotterie.getInstance().getBankAccountRepository();
 
 
     @Test
     public void testMessage() {
         
+        UserAccount userAccount = userAccountManager
+                .create("Dieter", "678", Constants.USER, Constants.CUSTOMER, Constants.CUSTOMER_BLOCKABLE);
+        userAccountManager.save(userAccount);
+        BankAccount bankAccount = new BankAccount();
+        bankAccountRepository.save(bankAccount);
+        
         ConcreteCustomer Customer1 = new ConcreteCustomer("Dieter", Status.ACTIVE, userAccount, bankAccount);
-
-
-        Lotterie.getInstance().getUserAccountManager().save(userAccount);
-
-
-        //Lotterie.getInstance().getCustomerRepository().save(Customer1);
+        customerRepository.save(Customer1);
         
         Customer1.addMessage(GameType.LOTTO);
         Customer1.addMessage(GameType.LOTTO);
@@ -39,7 +41,7 @@ public class MessageTest {
         
         //assertEquals(Customer1.getName(), "Dieter");
         
-//        assertEquals(Customer1.getMessageCount(), 0);   
+       assertEquals(Customer1.getMessageCount(), 5);   
 
 
 
